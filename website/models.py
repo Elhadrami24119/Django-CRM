10816a837f8e2baa6976df1d2fe32ccbfcf74481
Elhadrami24119 <24119@supnum.mr>
2026-03-06 16:26:33 +0000

    Implement CRM records: add models, migrations, views, URLs and templates for add_record and record pages

--- website/models.py
+++ website/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from django.db import models as ml
 
-# Create your models here.
+class Record(ml.Model):
+    created_at = ml.DateTimeField(auto_now_add=True)
+    first_name = ml.CharField(max_length=50)
+    last_name = ml.CharField(max_length=50)
+    email = ml.CharField(max_length=100)
+    phone = ml.CharField(max_length=15)
+    address = ml.CharField(max_length=100)
+    city = ml.CharField(max_length=50)
+    state = ml.CharField(max_length=50)
+    zipcode = ml.CharField(max_length=20)
+    
+
+    def __str__(self):
+        return(f"{self.first_name} {self.last_name}")
